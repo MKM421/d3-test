05d6a22f640c0c5ac6177c7921e18ec2fe253c0c
MichaelM <morganmichael340@gmail.com>
2016-09-21 11:50:43 -0700

    Change SVG height

--- js/main.js
+++ js/main.js
@@ -27,79 +27,69 @@ var runData = function() {
   }
 */
 
-  // width/height of SVG container
-  var w = 1200;
-  var h = 800;
+// width/height of SVG container
+var w = 1200;
+var h = 500;
 
-  // Selecting/creating SVG elements
-  var svg = d3.select("body")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
+// Selecting/creating SVG elements
+var svg = d3.select("body")
+.append("svg")
+.attr("width", w)
+.attr("height", h);
 
-  // Bind array to each Circle
-  var circles = svg.selectAll("circle")
-   .data(dataset)
-   .enter()
-   .append("circle");
-   circles.attr("cx", function(d, i) {
-      return (i * 30) + 0;
-    })
-      // add class to each circle
-      .attr("class", "cell")
-      .attr("cy", h/2)
-      //.attr("cx", h/2)
-      // increase radius 25X value
-      .attr("r", function(d) {
-        return d * 3;
-      })
-      .attr("fill", "rgba(0, 153, 153,0.3)")
-      .attr("stroke", "darkgrey")
-      .attr("stroke-width", function(d) {
-        return d/20;
-      })
-      .transition()
-      .delay(function(d, i) {
-        return i * 80;
-         //circles.call(d3.zoom().on("zoom", zoomed));
-      })
-    .duration(350)
-    .style("opacity", "1")
-    .style("color", function(d) {
-      if (d > 25) {	//Threshold of 15
-        return "darkgrey";
-      } else {
-        return "teal";
-      }
-    })
-      //.duration(1000)
-      //.attr("fill", "aqua")
-      svg.selectAll("text")
-      			   .data(dataset)
-      			   .enter()
-      			   .append("text")
-               .text(function(d){
-                 return d;
-               })
-               /*.
-               .text(function(d) {
-      			   		return d[0] + "," + d[1];
-      			   })
-      			   attr("x", function(d) {
-      			   		return d[0];
-      			   })
-      			   .attr("y", function(d) {
-      			   		return d[1];
-      			   })*/
-      			   .attr("font-family", "sans-serif")
-      			   .attr("font-size", "11px")
-      			   .attr("fill", "red");
+// Bind array to each Circle
+var circles = svg.selectAll("circle")
+  .data(dataset)
+  .enter()
+  .append("circle");
+  circles.attr("cx", function(d, i) {
+    return (i * 30) + 0;
+  })
+  // add class to each circle
+  .attr("class", "cell")
+  .attr("cy", h/2)
+  //.attr("cx", h/2)
+  // increase radius 25X value
+  .attr("r", function(d) {
+    return d * 3;
+  })
+  .attr("fill", "rgba(0, 153, 153,0.3)")
+  .attr("stroke", "darkgrey")
+  .attr("stroke-width", function(d) {
+    return d/20;
+  })
+  .transition()
+  .delay(function(d, i) {
+    return i * 80;
+  //circles.call(d3.zoom().on("zoom", zoomed));
+  })
+  .duration(350)
+  .style("opacity", "1");
+
+
+svg.selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text(function(d) {
+    return d[0] + "," + d[1];
+  })
+  .attr("x", function(d) {
+    return d[0];
+  })
+  .attr("y", function(d) {
+    return d[1];
+  })
+  .attr("class", "nums")
+  .attr("font-family", "sans-serif")
+  .attr("font-size", "11px")
+  .attr("fill", "white");
 
 
 
 
-  //console.log(d3.selectAll("li"));
-  console.log(dataset);
-  document.getElementById("nums").innerHTML = dataset;
+//console.log(d3.selectAll("li"));
+console.log(dataset);
+document.getElementById("nums").innerHTML = dataset;
 
 };
